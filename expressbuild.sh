25ab6ac73e8f3f75b54062f5f173c1244028832a
Ryan McCloskey <rmccloskey@tssg.org>
2022-10-17 14:09:50 +0100

    Added arm/v8 build

--- expressbuild.sh
+++ expressbuild.sh
@@ -1,3 +1,7 @@
+echo "### [BULDING ARMV8 $1 VERSION - HUB.DOCKER.COM] ###"
+docker buildx build --build-arg NUM=$1 --build-arg PLATFORM=armhf --platform linux/arm/v8 -t $2/expressvpn:$1-arm64 --push .
+echo "### [BULDING ARMV8 LATEST VERSION - HUB.DOCKER.COM] ###"
+docker buildx build --build-arg NUM=$1 --build-arg PLATFORM=armhf --platform linux/arm/v8 -t $2/expressvpn:latest-arm64 --push .
 echo "### [BULDING ARMHF $1 VERSION - HUB.DOCKER.COM] ###"
 docker buildx build --build-arg NUM=$1 --build-arg PLATFORM=armhf --platform linux/arm/v7 -t $2/expressvpn:$1-armhf --push .
 echo "### [BULDING ARMHF LATEST VERSION - HUB.DOCKER.COM] ###"
